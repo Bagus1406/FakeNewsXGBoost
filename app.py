93bd5b634f45df79e9b3b12405d1a8b894d0c90b
Bagus1406 <48698233+Bagus1406@users.noreply.github.com>
2025-05-28 15:12:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ Trained on 54k+ preprocessed English-language news samples.
                 st.subheader("Why this prediction?")
                 X_input = vectorizer.transform([user_input])
                 # Buat explainer hanya untuk input ini
-                explainer = shap.Explainer(model, X_input)
+                explainer = shap.Explainer(model.predict_proba, X_input
                 shap_values = explainer([X_input])
 
                 # Display SHAP bar chart
